d211648442dcdc07219e11785c88c4bb331a0581
morg1207 <alaurao@uni.pe>
2024-09-22 23:42:55 -0500

    First commit

--- build/quadruped_description/CMakeFiles/quadruped_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/quadruped_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/quadruped_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
